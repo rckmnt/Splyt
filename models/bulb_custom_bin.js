f3a43875a30f34faf7146ba8bdb98e03aa1bf280
commissionergordon <scottl318@gmail.com>
2016-07-31 20:54:20 +0200

    adds /models folder

---
+++ models/bulb_custom_bin.js
@@ -0,0 +1,62 @@
+{
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "bulb_custom.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 1441,
+        "faces"         : 2552,
+        "normals"       : 0,
+        "uvs"           : 507,
+        "materials"     : 4
+    },
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "mat3",
+	"colorDiffuse" : [0.8, 0.619618, 0.266],
+	"colorSpecular" : [0.5, 0.479069, 0.217079],
+	"illumination" : 2,
+	"opacity" : 1.0,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "mat2",
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opacity" : 1.0,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "mat1",
+	"colorDiffuse" : [0.381596, 0.381596, 0.381596],
+	"colorSpecular" : [0.114895, 0.114895, 0.114895],
+	"illumination" : 2,
+	"opacity" : 1.0,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "mat0.001",
+	"colorDiffuse" : [0.50265, 0.676033, 0.650105],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opacity" : 0.297521,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparent" : true
+	}],
+    "buffers": "bulb_custom_bin.bin"
+}
